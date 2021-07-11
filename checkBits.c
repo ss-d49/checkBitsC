65f9c0e7d79b7fe57865b91400bf102da9b938e1
S Day <52959996+ss-d49@users.noreply.github.com>
2021-07-11 14:24:45 +0100

    Update checkBits.c
    Fixed to allow for different header sizes.

--- checkBits.c
+++ checkBits.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-#define BYTES 2
+#define BYTES 1024
 int main (int argc, char **argv)
 {
   FILE *EXE;
   size_t bytes_in;
   unsigned char buf[BYTES];
+  unsigned char sub[2];
   const unsigned char header64[] = { 0x64, 0x86 };
   const unsigned char header86[] = { 0x4c, 0x01 };
 
@@ -20,7 +21,7 @@ int main (int argc, char **argv)
 
   if (EXE = fopen (argv[1], "rb"))
   {
-    fseek(EXE, 132, SEEK_SET);
+    fseek(EXE, 0, SEEK_SET);
     bytes_in = fread (buf, 1, BYTES, EXE);
     fclose (EXE);
   }
@@ -29,19 +30,23 @@ int main (int argc, char **argv)
     fprintf (stderr, "Error Opening exe file '%s': %s\n", argv[1], strerror (errno));
     exit(1);
   }
-
-  if (memcmp(buf, header64, BYTES) == 0)
-  {
-    printf("%s\n","File is 64 Bit.");
-  }
-  else if (memcmp(buf, header86, BYTES) == 0)
-  {
-    printf("%s\n","File is 32 Bit.");
-  }
-  else
-  {
-    printf("%s\n","File bitness is unknown.");
-  }
-
+  
+	for (int i=0;i<BYTES;i+=2) 
+	{
+		memcpy(sub, &buf[i], 2);	
+	
+	if (memcmp(sub, header64, 2) == 0)
+	  {
+		printf("%s\n","File is 64 Bit.");
+		  exit(0);
+	  }
+	else if (memcmp(sub, header86, 2) == 0)
+	  {
+		printf("%s\n","File is 32 Bit.");
+		  exit(0);
+	  }
+	}
+	
+	printf("%s\n","File bitness is unknown.");
   exit(0);
 }
